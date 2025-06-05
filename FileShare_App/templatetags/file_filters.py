a314fcd846e4b8c04904b3cef2f8f28ee1e0240c
EspaMC16 <markchristianespanol0516@gmail.com>
2025-06-05 17:12:05 +0800

    deleted a file

--- FileShare_App/templatetags/file_filters.py
+++
@@ -1,8 +0,0 @@
-from django import template
-
-register = template.Library()
-
-@register.filter
-def is_viewable(filename):
-    viewable_extensions = ['.pdf', '.jpg', '.jpeg', '.png', '.gif', '.txt']
-    return any(filename.lower().endswith(ext) for ext in viewable_extensions)
